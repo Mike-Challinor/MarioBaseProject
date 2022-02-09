5dde00ba360792119101b68970ea9c11aad8eb57
Michael Challinor <92083194+c026128l@users.noreply.github.com>
2022-02-09 17:27:03 +0000

    Tut 2 changes

--- TestApp/OpeningWindowTut2/OpeningWindowTut2.cpp
+++ TestApp/OpeningWindowTut2/OpeningWindowTut2.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
 #include "constants.h"
 
-int main()
+int main(int argc, char* args[])
 {
-    std::cout << "Hello World!\n";
+    return 0;
 }
--- TestApp/TestApp/Source.cpp
+++ TestApp/TestApp/Source.cpp
@@ -19,7 +19,29 @@ int main(int argc, char* args[])
 
 bool InitSDL()
 {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
 
+	else
+	{
+		//setup passed so create a window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+	}
 }
 
 void CloseSDL()
