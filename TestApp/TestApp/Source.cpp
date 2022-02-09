d915cdd24673918acddb29ea4d844d86ea4cebe3
Michael Challinor <92083194+c026128l@users.noreply.github.com>
2022-02-09 17:37:38 +0000

    Tut 2 completed

--- TestApp/TestApp/Source.cpp
+++ TestApp/TestApp/Source.cpp
@@ -14,15 +14,54 @@ void CloseSDL();
 
 int main(int argc, char* args[])
 {
+	//check if SDL was setup correctly
+	if (InitSDL())
+	{
+		SDL_Delay(5000);
+	}
+
+	CloseSDL();
+
 	return 0;
 }
 
 bool InitSDL()
 {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL did not initialise. Error: " << SDL_GetError();
+		return false;
+	}
+
+	else
+	{
+		//setup passed so create a window
+		g_window = SDL_CreateWindow("Games Engine Creation",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN);
+		//did the window get created?
+		if (g_window == nullptr)
+		{
+			//window failed
+			cout << "Window was not created. Error: " << SDL_GetError();
+			return false;
+		}
+	}
+
+	return true;
 
 }
 
 void CloseSDL()
 {
+	//release the window
+	SDL_DestroyWindow(g_window);
+	g_window = nullptr;
 
+	//quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
