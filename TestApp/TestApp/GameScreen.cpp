f89584108069c95d1289c844cb5729ed5ad148cd
Michael Challinor <92083194+c026128l@users.noreply.github.com>
2022-02-17 23:17:53 +0000

    created gamescreen class

---
+++ TestApp/TestApp/GameScreen.cpp
@@ -0,0 +1,18 @@
+#include "GameScreen.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <iostream>
+
+GameScreen::GameScreen(SDL_Renderer* renderer)
+{
+	m_renderer = renderer;
+}
+GameScreen::~GameScreen()
+{
+	m_renderer = nullptr;
+}
+
+void GameScreen::Render(){}
+
+void GameScreen::Update(float deltaTime, SDL_Event e){}
