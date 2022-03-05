47c9125417a20e67798136a3e51e4957b5313b42
Michael Challinor <92083194+c026128l@users.noreply.github.com>
2022-03-05 15:04:35 +0000

    setup levelmap header

--- TestApp/TestApp/constants.h
+++ TestApp/TestApp/constants.h
@@ -3,6 +3,8 @@
 //screen dimensions
 #define SCREEN_WIDTH 512
 #define SCREEN_HEIGHT 416
+#define MAP_HEIGHT 16
+#define MAP_WIDTH 13
 
 const float JUMP_FORCE_DECREMENT = 400.0f;
 const float INITIAL_JUMP_FORCE = 600.0f;
