93756645440e998994379c899d77882dc9e90515
Michael Challinor <92083194+c026128l@users.noreply.github.com>
2022-03-08 13:29:52 +0000

    header file for powblock

---
+++ TestApp/TestApp/PowBlock.cpp
@@ -0,0 +1,22 @@
+#include "PowBlock.h"
+
+PowBlock::PowBlock(SDL_Renderer* renderer, LevelMap* map)
+{
+	std::string imagePath = "Images/PowBlock.png"
+}
+
+PowBlock::~PowBlock()
+{
+
+}
+
+void PowBlock::Render()
+{
+
+}
+
+void PowBlock::TakeHit()
+{
+
+}
+
